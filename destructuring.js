d2f34560e71f634c84b5bf83a078ab481b126c9b
tw1ttt3r <tw1ttt3r@gmail.com>
2019-09-02 13:38:09 -0500

    uso de destructuring con variables

--- destructuring.js
+++ destructuring.js
@@ -3,3 +3,11 @@ let array=[100,200,300];
 let [cien,doscientos,trescientos] = array;
 
 console.log(cien,doscientos,trescientos);
+
+//destructuring variables
+let a=5;
+let b=1;
+console.log(a,b);
+[b,a]=[a,b];
+console.log(a,b);
+
