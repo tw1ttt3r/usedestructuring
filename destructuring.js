4fcc05f342e203476a2635267d9a1cc80e15782f
tw1ttt3r <tw1ttt3r@gmail.com>
2019-09-02 13:51:37 -0500

    uso de destructuring, rename variables

--- destructuring.js
+++ destructuring.js
@@ -22,3 +22,13 @@ console.log(objeto);
 let {id,name,active} = objeto;
 
 console.log(id,name,active);
+
+//destructuring objetos rename variables
+let persona={
+    folio:123,
+    nombre:"pedro fragoso",
+    edad:28
+};
+console.log(persona);
+let {folio:numPersona,nombre,edad:age}=persona;
+console.log(numPersona,nombre,age);
